916c4e343c823b846306fe9674dacf2276360355
zhangjun <1548293573@qq.com>
2024-04-14 20:12:14 +0800

    first commit

---
+++ src/components/SearchBox.vue
@@ -0,0 +1,20 @@
+/**
+  * SearchBox.vue TODO
+  * @Author ZhangJun
+  * @Date  2024/4/14 20:03
+  **/
+<template>
+  $END$
+</template>
+
+<script lang="ts">
+import {defineComponent} from 'vue'
+
+export default defineComponent({
+name: "SearchBox"
+})
+</script>
+
+<style scoped lang="less">
+
+</style>
---
+++ src/views/MainMap.vue
@@ -0,0 +1,20 @@
+/**
+  * MainMap.vue TODO
+  * @Author ZhangJun
+  * @Date  2024/4/14 19:27
+  **/
+<template>
+  $END$
+</template>
+
+<script lang="ts">
+import {defineComponent} from 'vue'
+
+export default defineComponent({
+name: "MainMap"
+})
+</script>
+
+<style scoped lang="less">
+
+</style>
